4c7672d1fa5aa642a381ff367c53c5e96246925c
Mr.Dangun <alsdnr9608@gmail.com>
2022-06-21 00:19:51 +0900

    Commit for "Modified" in VS code

---
+++ Clone-Coding/Youtube/main.js
@@ -0,0 +1,9 @@
+function print_hashtags () {
+    var hashtags = ['Apple', 'ApplePay', 'AppleCard'];
+
+    var i = 0;
+    while (i < hashtags.length) {
+        document.write('<li>'+hashtags[i]+'</li>')
+        i++;
+    }
+}
