b7d9cbbbe6907c3f8e4f1641dff1c3a719f64992
Dangun08 <minwook9608@gmail.com>
2022-07-01 17:45:51 +0900

    directory changed for index.html

---
+++ main.js
@@ -0,0 +1,9 @@
+function print_hashtags () {
+    var hashtags = ['Apple', 'ApplePay', 'AppleCard', 'Really?'];
+
+    var i = 0;
+    while (i < hashtags.length) {
+        document.write('<li>'+hashtags[i]+'</li>')
+        i++;
+    }
+}
